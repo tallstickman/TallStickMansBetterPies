765bf0d999076bf560ca3478eb77a8fb8586b631
TallStickMan <tallstickman@gmail.com>
2025-11-04 22:19:49 -0800

    Update block and item definitions to support 1.21.2

--- src/main/java/tv/tallstickman/betterpies/IceCream.java
+++ src/main/java/tv/tallstickman/betterpies/IceCream.java
@@ -4,57 +4,65 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 
 public class IceCream {
-    
-    // Vanilla -- The original
-	public static final FoodComponent VANILLA_ICE_CREAM_COMPONENT = (new FoodComponent.Builder()).nutrition(8).saturationModifier(0.3F).build();
-	public static final Item VANILLA_ICE_CREAM_ITEM = Registry.register(
-		Registries.ITEM,
-		Identifier.of(Constants.MODID_STRING, Constants.ITEM_VANILLA_ICE_CREAM_STRING),
-		new Item(new Item.Settings().food(VANILLA_ICE_CREAM_COMPONENT))
-	);
 
-	public static final FoodComponent BERRY_ICE_CREAM_COMPONENT = (new FoodComponent.Builder()).nutrition(8).saturationModifier(0.3F).build();
-	public static final Item BERRY_CREAM_ITEM = Registry.register(
-		Registries.ITEM,
-		Identifier.of(Constants.MODID_STRING, Constants.ITEM_BERRY_ICE_CREAM_STRING),
-		new Item(new Item.Settings().food(BERRY_ICE_CREAM_COMPONENT))
-	);
+	// Vanilla -- The original
+	public static final FoodComponent VANILLA_ICE_CREAM_COMPONENT = (new FoodComponent.Builder())
+			.nutrition(8)
+			.saturationModifier(0.3F)
+			.build();
+	public static final Item VANILLA_ICE_CREAM_ITEM = TallStickMansBetterPies.registerItem(
+			Constants.ITEM_VANILLA_ICE_CREAM_STRING,
+			Item::new,
+			new Item.Settings().food(VANILLA_ICE_CREAM_COMPONENT));
 
-	public static final FoodComponent CHOCOLATE_ICE_CREAM_COMPONENT = (new FoodComponent.Builder()).nutrition(8).saturationModifier(0.3F).build();
-	public static final Item CHOCOLATE_ICE_CREAM_ITEM = Registry.register(
-		Registries.ITEM,
-		Identifier.of(Constants.MODID_STRING, Constants.ITEM_CHOCOLATE_ICE_CREAM_STRING),
-		new Item(new Item.Settings().food(CHOCOLATE_ICE_CREAM_COMPONENT))
-	);
+	public static final FoodComponent BERRY_ICE_CREAM_COMPONENT = (new FoodComponent.Builder())
+			.nutrition(8)
+			.saturationModifier(0.3F)
+			.build();
+	public static final Item BERRY_CREAM_ITEM = TallStickMansBetterPies.registerItem(
+			Constants.ITEM_BERRY_ICE_CREAM_STRING,
+			Item::new,
+			new Item.Settings().food(BERRY_ICE_CREAM_COMPONENT));
 
-	public static final FoodComponent NEAPOLITAN_ICE_CREAM_COMPONENT = (new FoodComponent.Builder()).nutrition(8).saturationModifier(0.3F).build();
-	public static final Item NEAPOLITAN_ICE_CREAM_ITEM = Registry.register(
-		Registries.ITEM,
-		Identifier.of(Constants.MODID_STRING, Constants.ITEM_NEAPOLITAN_ICE_CREAM_STRING),
-		new Item(new Item.Settings().food(NEAPOLITAN_ICE_CREAM_COMPONENT))
-	);
+	public static final FoodComponent CHOCOLATE_ICE_CREAM_COMPONENT = (new FoodComponent.Builder())
+			.nutrition(8)
+			.saturationModifier(0.3F)
+			.build();
+	public static final Item CHOCOLATE_ICE_CREAM_ITEM = TallStickMansBetterPies.registerItem(
+			Constants.ITEM_CHOCOLATE_ICE_CREAM_STRING,
+			Item::new,
+			new Item.Settings().food(CHOCOLATE_ICE_CREAM_COMPONENT));
 
-	public static final FoodComponent COOKIES_AND_CREAM_ICE_CREAM_COMPONENT = (new FoodComponent.Builder()).nutrition(8).saturationModifier(0.3F).build();
-	public static final Item COOKIES_AND_CREAM_ICE_CREAM_ITEM = Registry.register(
-		Registries.ITEM,
-		Identifier.of(Constants.MODID_STRING, Constants.ITEM_COOKIES_AND_CREAM_ICE_CREAM_STRING),
-		new Item(new Item.Settings().food(COOKIES_AND_CREAM_ICE_CREAM_COMPONENT))
-	);
+	public static final FoodComponent NEAPOLITAN_ICE_CREAM_COMPONENT = (new FoodComponent.Builder())
+			.nutrition(8)
+			.saturationModifier(0.3F)
+			.build();
+	public static final Item NEAPOLITAN_ICE_CREAM_ITEM = TallStickMansBetterPies.registerItem(
+			Constants.ITEM_NEAPOLITAN_ICE_CREAM_STRING,
+			Item::new,
+			new Item.Settings().food(NEAPOLITAN_ICE_CREAM_COMPONENT));
 
-	public static final FoodComponent MINT_CHIP_ICE_CREAM_COMPONENT = (new FoodComponent.Builder()).nutrition(8).saturationModifier(0.3F).build();
-	public static final Item MINT_CHIP_ICE_CREAM_ITEM = Registry.register(
-		Registries.ITEM,
-		Identifier.of(Constants.MODID_STRING, Constants.ITEM_MINT_CHIP_ICE_CREAM_STRING),
-		new Item(new Item.Settings().food(MINT_CHIP_ICE_CREAM_COMPONENT))
-	);
+	public static final FoodComponent COOKIES_AND_CREAM_ICE_CREAM_COMPONENT = (new FoodComponent.Builder())
+			.nutrition(8)
+			.saturationModifier(0.3F)
+			.build();
+	public static final Item COOKIES_AND_CREAM_ICE_CREAM_ITEM = TallStickMansBetterPies.registerItem(
+			Constants.ITEM_COOKIES_AND_CREAM_ICE_CREAM_STRING,
+			Item::new,
+			new Item.Settings().food(COOKIES_AND_CREAM_ICE_CREAM_COMPONENT));
 
-	public static void initialize()
-	{
+	public static final FoodComponent MINT_CHIP_ICE_CREAM_COMPONENT = (new FoodComponent.Builder())
+			.nutrition(8)
+			.saturationModifier(0.3F)
+			.build();
+	public static final Item MINT_CHIP_ICE_CREAM_ITEM = TallStickMansBetterPies.registerItem(
+			Constants.ITEM_MINT_CHIP_ICE_CREAM_STRING,
+			Item::new,
+			new Item.Settings().food(MINT_CHIP_ICE_CREAM_COMPONENT));
+
+	public static void initialize() {
 		// Add food items to the "Food and Drink" Item Group
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(content -> {
 			content.add(VANILLA_ICE_CREAM_ITEM);
--- src/main/java/tv/tallstickman/betterpies/OtherFoods.java
+++ src/main/java/tv/tallstickman/betterpies/OtherFoods.java
@@ -4,15 +4,18 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 
 public class OtherFoods {
     
     // Cheese used for making Cheesecake
-	public static final FoodComponent CHEESE_FOOD_COMPONENT = (new FoodComponent.Builder()).nutrition(4).saturationModifier(0.6F).build();
-	public static final Item CHEESE_ITEM = Registry.register(Registries.ITEM, Identifier.of(Constants.MODID_STRING, Constants.ITEM_CHEESE_STRING), new DairyItem(new Item.Settings().food(CHEESE_FOOD_COMPONENT)));
+	public static final FoodComponent CHEESE_FOOD_COMPONENT = (new FoodComponent.Builder())
+	.nutrition(4)
+	.saturationModifier(0.6F)
+	.build();
+	public static final Item CHEESE_ITEM = TallStickMansBetterPies.registerItem(
+		Constants.ITEM_CHEESE_STRING,
+		DairyItem::new,
+		new Item.Settings().food(CHEESE_FOOD_COMPONENT));
 
 	public static void initialize()
 	{
