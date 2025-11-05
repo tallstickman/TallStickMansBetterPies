61cf8e65102357b3528e33f6158ef131ef5e9cc0
TallStickMan <tallstickman@gmail.com>
2025-11-04 22:19:49 -0800

    Update block and item definitions to support 1.21.2

--- src/main/java/tv/tallstickman/betterpies/Fruits.java
+++ src/main/java/tv/tallstickman/betterpies/Fruits.java
@@ -21,9 +21,6 @@ import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
-import net.minecraft.item.BlockItem;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
@@ -33,43 +30,75 @@ import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.gen.GenerationStep;
 
 public class Fruits {
-    
-    // Blackberries for blackberry pie.
-	//   This block is initialized in the main mod initializer so that its declaration here can be referenced by the related item here.
-	public static final Block BLACKBERRY_BUSH_BLOCK = new BlackberryBushBlock(AbstractBlock.Settings.create().mapColor(MapColor.DARK_GREEN).ticksRandomly().noCollision().sounds(BlockSoundGroup.SWEET_BERRY_BUSH).pistonBehavior(PistonBehavior.DESTROY).nonOpaque());
-	public static final FoodComponent BLACKBERRIES_FOOD_COMPONENT = (new FoodComponent.Builder()).nutrition(2).saturationModifier(0.1F).build();
-	public static final Item BLACKBERRIES_ITEM = Registry.register(Registries.ITEM, Identifier.of(Constants.MODID_STRING, Constants.ITEM_BLACKBERRIES_STRING), new BlockItem(BLACKBERRY_BUSH_BLOCK, new Item.Settings().food(BLACKBERRIES_FOOD_COMPONENT)));
 
-    // Blueberries for blueberry pie.
-	//   This block is initialized in the main mod initializer so that its declaration here can be referenced by the related item here.
-	public static final Block BLUEBERRY_BUSH_BLOCK = new BlueberryBushBlock(AbstractBlock.Settings.create().mapColor(MapColor.DARK_GREEN).ticksRandomly().noCollision().sounds(BlockSoundGroup.SWEET_BERRY_BUSH).pistonBehavior(PistonBehavior.DESTROY).nonOpaque());
-	public static final FoodComponent BLUEBERRIES_FOOD_COMPONENT = (new FoodComponent.Builder()).nutrition(2).saturationModifier(0.1F).build();
-	public static final Item BLUEBERRIES_ITEM = Registry.register(Registries.ITEM, Identifier.of(Constants.MODID_STRING, Constants.ITEM_BLUEBERRIES_STRING), new BlockItem(BLUEBERRY_BUSH_BLOCK, new Item.Settings().food(BLUEBERRIES_FOOD_COMPONENT)));
-	
-    // Cherries for cherry pie.
-	public static final FoodComponent CHERRIES_FOOD_COMPONENT = (new FoodComponent.Builder()).nutrition(4).saturationModifier(0.3F).build();
-	public static final Item CHERRIES_ITEM = Registry.register(Registries.ITEM, Identifier.of(Constants.MODID_STRING, Constants.ITEM_CHERRIES_STRING), new Item(new Item.Settings().food(CHERRIES_FOOD_COMPONENT)));
+	// Blackberries for blackberry pie.
+	// This block is initialized in the main mod initializer so that its declaration
+	// here can be referenced by the related item here.
+	public static final Block BLACKBERRY_BUSH_BLOCK = TallStickMansBetterPies.registerBlock(
+			Constants.BLOCK_BLACKBERRY_BUSH_STRING,
+			BlackberryBushBlock::new,
+			AbstractBlock.Settings.create()
+					.mapColor(MapColor.DARK_GREEN)
+					.ticksRandomly()
+					.noCollision()
+					.sounds(BlockSoundGroup.SWEET_BERRY_BUSH)
+					.pistonBehavior(PistonBehavior.DESTROY)
+					.nonOpaque());
+	public static final FoodComponent BLACKBERRIES_FOOD_COMPONENT = (new FoodComponent.Builder())
+			.nutrition(2)
+			.saturationModifier(0.1F)
+			.build();
+	public static final Item BLACKBERRIES_ITEM = TallStickMansBetterPies.registerBlockItem(
+			Constants.ITEM_BLACKBERRIES_STRING,
+			BLACKBERRY_BUSH_BLOCK,
+			new Item.Settings().food(BLACKBERRIES_FOOD_COMPONENT));
 
-	public static void initialize()
-	{
+	// Blueberries for blueberry pie.
+	// This block is initialized in the main mod initializer so that its declaration
+	// here can be referenced by the related item here.
+	public static final Block BLUEBERRY_BUSH_BLOCK = TallStickMansBetterPies.registerBlock(
+			Constants.BLOCK_BLUEBERRY_BUSH_STRING,
+			BlueberryBushBlock::new,
+			AbstractBlock.Settings.create()
+					.mapColor(MapColor.DARK_GREEN)
+					.ticksRandomly()
+					.noCollision()
+					.sounds(BlockSoundGroup.SWEET_BERRY_BUSH)
+					.pistonBehavior(PistonBehavior.DESTROY)
+					.nonOpaque());
+	public static final FoodComponent BLUEBERRIES_FOOD_COMPONENT = (new FoodComponent.Builder())
+			.nutrition(2)
+			.saturationModifier(0.1F)
+			.build();
+	public static final Item BLUEBERRIES_ITEM = TallStickMansBetterPies.registerBlockItem(
+			Constants.ITEM_BLUEBERRIES_STRING,
+			BLUEBERRY_BUSH_BLOCK,
+			new Item.Settings().food(BLUEBERRIES_FOOD_COMPONENT));
+
+	// Cherries for cherry pie.
+	public static final FoodComponent CHERRIES_FOOD_COMPONENT = (new FoodComponent.Builder())
+			.nutrition(4)
+			.saturationModifier(0.3F)
+			.build();
+	public static final Item CHERRIES_ITEM = TallStickMansBetterPies.registerItem(
+			Constants.ITEM_CHERRIES_STRING,
+			Item::new,
+			new Item.Settings().food(CHERRIES_FOOD_COMPONENT));
+
+	public static void initialize() {
 		// Add cherry drops to the Minecraft Cherry Blossom Leaves block.
 		final Optional<RegistryKey<LootTable>> CHERRY_LEAVES_LOOT_TABLE_ID = Blocks.CHERRY_LEAVES.getLootTableKey();
 		LootTableEvents.MODIFY.register((id, tableBuilder, source, wrapper) -> {
-			if (source.isBuiltin() && CHERRY_LEAVES_LOOT_TABLE_ID.isPresent() && CHERRY_LEAVES_LOOT_TABLE_ID.get().equals(id)) {
+			if (source.isBuiltin() && CHERRY_LEAVES_LOOT_TABLE_ID.isPresent()
+					&& CHERRY_LEAVES_LOOT_TABLE_ID.get().equals(id)) {
 				LootPool.Builder poolBuilder = LootPool.builder()
-				.rolls(UniformLootNumberProvider.create(1, 3))
-				.conditionally(RandomChanceLootCondition.builder(0.05F))
-                .with(ItemEntry.builder(Fruits.CHERRIES_ITEM));
- 
-                tableBuilder.pool(poolBuilder.build());
-		
-            }
-        });
+						.rolls(UniformLootNumberProvider.create(1, 3))
+						.conditionally(RandomChanceLootCondition.builder(0.05F))
+						.with(ItemEntry.builder(Fruits.CHERRIES_ITEM));
 
-		// Register new berry bush blocks.
-		Registry.register(Registries.BLOCK, Identifier.of(Constants.MODID_STRING, Constants.BLOCK_BLUEBERRY_BUSH_STRING), Fruits.BLUEBERRY_BUSH_BLOCK);
-		Registry.register(Registries.BLOCK, Identifier.of(Constants.MODID_STRING, Constants.BLOCK_BLACKBERRY_BUSH_STRING), Fruits.BLACKBERRY_BUSH_BLOCK);
-		
+				tableBuilder.pool(poolBuilder.build());}
+		});
+
 		// Register new fruits as compostable.
 		CompostingChanceRegistry.INSTANCE.add(BLACKBERRIES_ITEM, 0.3f);
 		CompostingChanceRegistry.INSTANCE.add(BLUEBERRIES_ITEM, 0.3f);
@@ -77,31 +106,29 @@ public class Fruits {
 
 		// Create and register berry patch terrain Features
 		final List<RegistryKey<Biome>> BLUEBERRY_BIOMES = List.of(
-			BiomeKeys.WINDSWEPT_GRAVELLY_HILLS,
-			BiomeKeys.WINDSWEPT_HILLS,
-			BiomeKeys.WINDSWEPT_FOREST,
-			BiomeKeys.SNOWY_TAIGA,
-			BiomeKeys.SNOWY_SLOPES,
-			BiomeKeys.TAIGA,
-			BiomeKeys.OLD_GROWTH_PINE_TAIGA,
-			BiomeKeys.OLD_GROWTH_SPRUCE_TAIGA
-		);
-        BiomeModifications.addFeature(
-			BiomeSelectors.includeByKey(BLUEBERRY_BIOMES),
-			GenerationStep.Feature.VEGETAL_DECORATION,
-			RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(Constants.MODID_STRING, Constants.PLACED_FEATURE_BLUEBERRY_BUSH_PATCH_STRING))
-		);
+				BiomeKeys.WINDSWEPT_GRAVELLY_HILLS,
+				BiomeKeys.WINDSWEPT_HILLS,
+				BiomeKeys.WINDSWEPT_FOREST,
+				BiomeKeys.SNOWY_TAIGA,
+				BiomeKeys.SNOWY_SLOPES,
+				BiomeKeys.TAIGA,
+				BiomeKeys.OLD_GROWTH_PINE_TAIGA,
+				BiomeKeys.OLD_GROWTH_SPRUCE_TAIGA);
+		BiomeModifications.addFeature(
+				BiomeSelectors.includeByKey(BLUEBERRY_BIOMES),
+				GenerationStep.Feature.VEGETAL_DECORATION,
+				RegistryKey.of(RegistryKeys.PLACED_FEATURE,
+						Identifier.of(Constants.MODID_STRING, Constants.PLACED_FEATURE_BLUEBERRY_BUSH_PATCH_STRING)));
 		final List<RegistryKey<Biome>> BLACKBERRY_BIOMES = List.of(
-			BiomeKeys.SAVANNA,
-			BiomeKeys.SAVANNA_PLATEAU,
-			BiomeKeys.WINDSWEPT_SAVANNA,
-			BiomeKeys.WOODED_BADLANDS
-		);
-         BiomeModifications.addFeature(
-			BiomeSelectors.includeByKey(BLACKBERRY_BIOMES),
-			GenerationStep.Feature.VEGETAL_DECORATION,
-			RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(Constants.MODID_STRING, Constants.PLACED_FEATURE_BLACKBERRY_BUSH_PATCH_STRING))
-		);
+				BiomeKeys.SAVANNA,
+				BiomeKeys.SAVANNA_PLATEAU,
+				BiomeKeys.WINDSWEPT_SAVANNA,
+				BiomeKeys.WOODED_BADLANDS);
+		BiomeModifications.addFeature(
+				BiomeSelectors.includeByKey(BLACKBERRY_BIOMES),
+				GenerationStep.Feature.VEGETAL_DECORATION,
+				RegistryKey.of(RegistryKeys.PLACED_FEATURE,
+						Identifier.of(Constants.MODID_STRING, Constants.PLACED_FEATURE_BLACKBERRY_BUSH_PATCH_STRING)));
 
 		// Add food items to the "Food and Drink" Item Group
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(content -> {
@@ -110,5 +137,7 @@ public class Fruits {
 			content.add(Fruits.BLUEBERRIES_ITEM);
 			content.add(Fruits.CHERRIES_ITEM);
 		});
+
 	}
+
 }
--- src/main/java/tv/tallstickman/betterpies/TallStickMansBetterPies.java
+++ src/main/java/tv/tallstickman/betterpies/TallStickMansBetterPies.java
@@ -1,6 +1,16 @@
 package tv.tallstickman.betterpies;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,22 +18,21 @@ import org.slf4j.LoggerFactory;
 public class TallStickMansBetterPies implements ModInitializer {
 
 	// Activate the static members of the various classes of foods.
-	public static final Fruits FRUITS          = new Fruits();
-	public static final Pies PIES              = new Pies();
+	public static final Fruits FRUITS = new Fruits();
+	public static final Pies PIES = new Pies();
 	public static final OtherFoods OTHER_FOODS = new OtherFoods();
-	public static final IceCream ICE_CREAM     = new IceCream();
+	public static final IceCream ICE_CREAM = new IceCream();
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MODID_STRING);
+	public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MODID_STRING);
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.#
 		// However, some things (like resources) may still be uninitialized.s
 		// Proceed with mild caution.
-
 
 		LOGGER.info("Growing new fruits...");
 		Fruits.initialize();
@@ -34,4 +43,50 @@ public class TallStickMansBetterPies implements ModInitializer {
 		LOGGER.info("Churning up ice cream...");
 		IceCream.initialize();
 	}
+
+	// Factory methods for fruit items and blocks
+
+	public static Block registerBlock(String name, Function<Block.Settings, Block> blockFactory, Block.Settings settings) {
+		// Create the block key.
+		RegistryKey<Block> blockKey = keyOfBlock(name);
+		// Create the block instance
+		Block block = blockFactory.apply(settings.registryKey(blockKey));
+
+		return Registry.register(Registries.BLOCK, blockKey, block);
+	}
+
+	// Block registerBlockItem(String blockName, String itemName, Function<Block.Settings, Block> blockFactory, Block.Settings settings) {
+	// 	return this.registerBlockItem(blockName, itemName, blockFactory, settings, true);
+	// }
+
+	public static Item registerBlockItem(String itemName, Block block, Item.Settings settings) {
+			// Items need to be registered with a different type of registry key, but the ID
+			// can be the same.
+			RegistryKey<Item> itemKey = keyOfItem(itemName);
+
+			BlockItem blockItem = new BlockItem(block, new Item.Settings().registryKey(itemKey));
+			return Registry.register(Registries.ITEM, itemKey, blockItem);
+	}
+
+	public static Item registerItem(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+		// Create the item key.
+		RegistryKey<Item> itemKey = keyOfItem(name);
+
+		// Create the item instance.
+		Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+		// Register the item.
+		Registry.register(Registries.ITEM, itemKey, item);
+
+		return item;
+	}
+
+	private static RegistryKey<Block> keyOfBlock(String name) {
+		return RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(Constants.MODID_STRING, name));
+	}
+
+	private static RegistryKey<Item> keyOfItem(String name) {
+		return RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Constants.MODID_STRING, name));
+	}
+
 }
